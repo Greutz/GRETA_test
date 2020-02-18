0a86e5b6280ab9e54ab15894cac4d82284c71dd0
Greutz <gorton83@gmail.com>
2020-02-18 16:29:47 +0100

    added stuff

--- js/map.js
+++ js/map.js
@@ -1,17 +1,10 @@
-
-// Initialize the platform object:
-var platform = new H.service.Platform({
-    'apikey': '{E-TtPSQvuEIsvIsOzTqA4XxBpI2kaigT46zBgzmWlOs}'
-  });
-
-// Obtain the default map types from the platform object
-var defaultLayers = platform.createDefaultLayers();
-
-// Instantiate (and display) a map object:
-var map = new H.Map(
-  document.getElementById('mapContainer'),
-  maptypes.vector.normal.map,
-  {
-    zoom: 10,
-    center: { lng: 13.4, lat: 52.51 }
-  });
+// Initialize and add the map
+function initMap() {
+  // The location of toulon
+  var toulon = {lat: 43.124228, lng: 5.928};
+  // The map, centered at toulon
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 14, center: toulon});
+  // The marker, positioned at toulon
+  var marker = new google.maps.Marker({position: toulon, map: map});
+}
